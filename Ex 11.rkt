2c59e5c1919940976c2100b47ef3f76be7b4f9bb
Raivat Shah <raivat@u.nus.edu>
2018-12-28 22:33:54 +0530

    Added exercises

---
+++ Ex 11.rkt
@@ -0,0 +1,6 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |Ex 11|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(define (hyp x y) (sqrt (+ (sqr x)(sqr y))))
+(hyp 3 4)
+
